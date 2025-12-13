dba890bd15c49d6f507d41f01fd52cbd051bce33
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-13 23:38:13 +0530

    access and refresh tokens flow

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,44 @@
-import { createUser } from "../services/auth.service.js";
+import { createUser } from '../services/auth.service.js';
 
 export const register = async (req, res, next) => {
   try {
-        const { name, email, picture, status, password } = req.body;
-        const newUser = await createUser({
-          name,
-          email,
-          picture,
-          status,
-          password,
-        });
+    const { name, email, picture, status, password } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      status,
+      password,
+    });
 
-        res.json(newUser);
+    const access_token = await generateToken(
+      { userId: newUser._id },
+      '1d',
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const refresh_token = await generateToken(
+      { userId: newUser._id },
+      '30d',
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie('refreshtoken', refresh_token, {
+      httpOnly: true,
+      path: '/api/v1/auth/refreshtoken',
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
+
+    res.json({
+      message: 'register success.',
+      access_token,
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        picture: newUser.picture,
+        status: newUser.status,
+      },
+    });
   } catch (error) {
     next(error);
   }
