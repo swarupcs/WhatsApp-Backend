7489c41cf00ec7f2c40967d3df1dd67b3bb85e6c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-13 23:29:25 +0530

    server and http errors handling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,34 @@ import app from './app.js';
 //env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  // console.log(`server is listening at ${PORT}...`);
-   logger.info(`Server is listening at ${PORT}...`);
+let server;
+
+server = app.listen(PORT, () => {
+  logger.info(`Server is listening at ${PORT}...`);
+});
+
+//handle server errors
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
