ef95d4831504752bca4d4d07b2faba70db83072c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-14 18:13:45 +0530

    search users

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import authRoutes from './auth.route.js';
 import ConversationRoutes from './conversation.route.js';
 import MessageRoutes from './message.route.js';
+import userROutes from './user.route.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/conversation', ConversationRoutes);
 router.use('/message', MessageRoutes);
+router.use('/user', userROutes);
+
+
 export default router;
