2704ceebff2782cb6bb0a866fc31b27971f066c5
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-14 17:48:22 +0530

    open or create a conversation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import authRoutes from './auth.route.js';
+import ConversationRoutes from './conversation.route.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/conversation', ConversationRoutes);
 export default router;
